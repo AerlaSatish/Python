efdb46a304c5aeb48014876dfab54d6225de9625
aerla <satish.aerla@launchship.com>
2018-07-27 17:55:19 +0530

    First Attempt

---
+++ beginners/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for beginners project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+"""
+"""
+Testing for git
+"""
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beginners.settings')
+
+application = get_wsgi_application()
